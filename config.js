435302df1d7b147fe77fca0d983c9ed4b147c4ba
RudraThakkar-code <rudrathehero111@gmail.com>
2025-01-18 15:50:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    titleWeb: "Princy Thakkar",
+    introTitle: 'Happyy Bithday Mere bacho ki Maa',
+    introDesc: `Kitni ajeeb baat hai yeh ki aap kehte ki kisi aur se pyaar kar aur pehle aap chate thae
+    ki sirf mere se kar woh shagun ka msg dekhke leg ko apne control meh karna 🤣🤣 mujhe abhi bhi yaad hai
+    pehle aap chate thae abh meh apako chata hu isliye sirf aapse pyaaar karuga🫂🤞
+    HAPPY BIRTHDAY MY ONLY LOVE🫀`,
+    btnIntro: '^^Kese ho^^',
+    title: 'Wese kesa lagh raha hai yeh jo birthday ke kuch din pehle ki excitement hai ki nahi ya meh kardu finger se😏😏',
+    desc: 'Wese jabse apke lips mere hue hai tabse glow karne lagae hai mere lips my lips are feeling special now🤣🤣💋',
+    btnYes: 'Again fell in your LOVE 😍 <33',
+    btnNo: 'Wese apka ans no hoga but theek hai zidd karna ata hai mujhe:3',
+    question: 'Wese aabhi bhi reason mila kya ki Puri duniya ke 7 billion people on Earth, 7 continent, 7 seas, 196 countries So why did you chosed me<3',
+    btnReply: 'But for me I the most happy and blessed soul becuase in this difficult odd I am in Love with YOU😁😁',
+    reply: 'Kuch nahi bass gilli chummi dedo <33333333',
+    mess: 'Soo what do you say now excited for the BIrthdayyyy!!🥳🥳',
+    messDesc: 'Aaogae Room meh chal kare Great Grand Masti🤣🤣Na Na Apan masti karege but normal wali kyuki Guest hai badme karege apni masti😏😏',
+    btnAccept: 'Okey-Dokey <3',
+    messLink: 'https://Rudrincy.com' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
